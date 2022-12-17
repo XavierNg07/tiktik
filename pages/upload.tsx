3ddf9855c5ef710f1ae2a229f4faedb8628d961d
xavier <xaviernguyen07@gmail.com>
2022-12-17 14:49:42 -0800

    fixing auth

---
+++ pages/upload.tsx
@@ -0,0 +1,15 @@
+import React, {useState, useEffect} from 'react';
+import {useRouter} from 'next/router';
+import {FaCloudUploadAlt} from 'react-icons/fa';
+import {MdDelete} from 'react-icons/md';
+import axios from 'axios';
+import useAuthStore from '../store/authStore';
+import {client} from '../utils/client';
+
+export default function Upload(){
+    return (
+      <div>
+
+      </div>
+    );
+}
--- store/authStore.ts
+++ store/authStore.ts
@@ -1,11 +1,10 @@
 import create from 'zustand';
 import {persist} from 'zustand/middleware';
 
-const authStore = (set: any) => ({
-    userProfile: null,
-    addUser: (user : any) => set({userProfile: user}),
-    removeUser: () => set({userProfile : null})
+const authStore = (set : any) => ({
+    userProfile : null,
+    addUser: (user : any) => set({userProfile : user}),
+    removeUser: () => set({userProfile : null}),
 });
 
-const useAuthStore = create(persist(authStore, {name: 'auth'}))
-export default useAuthStore;
+export default create(persist(authStore, {name: 'auth'}))
