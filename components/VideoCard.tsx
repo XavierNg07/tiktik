cae2aa6c3895d6e565bae897c2975f54f6d9c425
xavier <xaviernguyen07@gmail.com>
2022-12-16 22:07:23 -0800

    added video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -6,6 +6,20 @@ import {HiVolumeUp, HiVolumeOff} from 'react-icons/hi';
 import {BsFillPlayFill, BsFillPauseFill} from 'react-icons/all';
 import {GoVerified} from 'react-icons/go';
 export default function VideoCard({post} : {post : Video}){
+    const [isHover, setIsHover] = useState(false);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isMuted, setIsMuted] = useState(false);
+    const videoRef = useRef<HTMLVideoElement>(null);
+
+    const onVideoPress = () => {
+        if (isPlaying){
+            videoRef?.current?.pause();
+            setIsPlaying(false);
+        } else {
+            videoRef?.current?.play().then(() => {setIsPlaying(true)});
+        }
+    }
+
     return (
         <div className="flex flex-col border-b-2 border-gray-200 pb-6">
             <div>
@@ -20,14 +34,43 @@ export default function VideoCard({post} : {post : Video}){
                     </div>
                     <div>
                         <Link href="/">
-                            <div>
-                                <p>
+                            <div className="flex items-center gap-2">
+                                <p className="flex gap-2 items-center md:text-md font-bold text-primary">
                                     {post.postedBy.name} {``}
-                                    <GoVerified className="text-blue-400-text-md"/>
+                                    <GoVerified className="text-blue-400 text-md"/>
+                                </p>
+                                <p className="capitalize font-medium text-xs text-gray-500 hidden md:block">
+                                    {post.postedBy.name}
                                 </p>
                             </div>
                         </Link>
                     </div>
+                </div>
+            </div>
+            <div className="lg:ml-20 flex gap-4 relative">
+                <div onMouseEnter={() => {setIsHover(true)}}
+                     onMouseLeave={() => {setIsHover(false)}}
+                     className="rounded-3xl">
+                    <Link href="/">
+                        <video loop src={post.video.asset.url}
+                               ref={videoRef}
+                               className="lg:w[600px] h-[300px] md:h-[400px] lg:h-[530px] w-[200px] rounded-2xl cursor-pointer bg-gray-100">
+                        </video>
+                    </Link>
+                    {isHover && (
+                        <div>
+                            {isPlaying ? (
+                                <button onClick={onVideoPress}><BsFillPauseFill className="text-black text-2xl lg:text-4xl"/></button>
+                            ) : (
+                                <button onClick={onVideoPress}><BsFillPlayFill className="text-black text-2xl lg:text-4xl"/></button>
+                            )}
+                            {isMuted ? (
+                                <button onClick={() => {setIsMuted(false)}}><HiVolumeOff className="text-black text-2xl lg:text-4xl"/></button>
+                            ) : (
+                                <button onClick={() => {setIsMuted(true)}}><HiVolumeUp className="text-black text-2xl lg:text-4xl"/></button>
+                            )}
+                        </div>
+                    )}
                 </div>
             </div>
         </div>
