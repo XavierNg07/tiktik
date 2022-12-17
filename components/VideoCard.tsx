d7e292634211a90b94ba76ba06e5ae5e5d1df039
xavier <xaviernguyen07@gmail.com>
2022-12-16 22:10:21 -0800

    edit button for video

--- components/VideoCard.tsx
+++ components/VideoCard.tsx
@@ -58,7 +58,7 @@ export default function VideoCard({post} : {post : Video}){
                         </video>
                     </Link>
                     {isHover && (
-                        <div>
+                        <div className="absolute bottom-6 cursor-pointer left-8 md:left-14 lg:left-0 flex gap-10 lg:justify-between w-[100px] md:w-[50px] p-3">
                             {isPlaying ? (
                                 <button onClick={onVideoPress}><BsFillPauseFill className="text-black text-2xl lg:text-4xl"/></button>
                             ) : (
