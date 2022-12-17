1d356fb18791ff6f113a95b8c19e992822cc605b
xavier <xaviernguyen07@gmail.com>
2022-12-16 16:40:30 -0800

    debug tailwindcss

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -14,15 +14,15 @@ export default function App({ Component, pageProps }: AppProps) {
 
   return (
       <>
-        <Navbar/>
-        <div className="flex gap-6 md:gap-20">
-            <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
-                <Sidebar/>
-            </div>
-            <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
-                <Component {...pageProps} />
-            </div>
-        </div>
+          <Navbar/>
+          <div className="flex gap-6 md:gap-20">
+              <div className="h-[92vh] overflow-hidden xl:hover:overflow-auto">
+                  <Sidebar/>
+              </div>
+              <div className="mt-4 flex flex-col gap-10 overflow-auto h-[88vh] videos flex-1">
+                  <Component {...pageProps} />
+              </div>
+          </div>
       </>
   );
 }
