78457ca51eecad2489df0ef6fc805abf6c19892b
xavier <xaviernguyen07@gmail.com>
2022-12-17 21:33:59 -0800

    partial detail page2

--- pages/detail/[id].tsx
+++ pages/detail/[id].tsx
@@ -11,11 +11,23 @@ import {BASE_URL} from '../../utils';
 import {Video} from '../../types';
 export default function Detail({postDetails} : {postDetails : Video}){
     const [post, setPost] = useState(postDetails);
-    const videoRef = useRef(null);
+    const videoRef = useRef<HTMLVideoElement>(null);
+    const [isPlaying, setIsPlaying] = useState(false);
+
+    function playVid(){
+        if (isPlaying){
+            videoRef?.current?.pause();
+            setIsPlaying(false);
+        } else {
+            videoRef?.current?.play().then(() => {setIsPlaying(true)});
+        }
+    }
+
     if (!post) return null;
     return (
         <div className={"flex w-full absolute left-0 top-0 bg-white flex-wrap lg:flex-nowrap"}>
-            <div className={"relative flex-2 w-[1000px] lg:w-9/12 flex justify-center items-center bg-black-600"}>
+            <div className={"relative flex-2 w-[1000px] lg:w-9/12 flex justify-center " +
+                "items-center bg-black"}>
                 <div className={"absolute top-6 left-2 lg:left-6 flex gap-6 z-50"}>
                     <p>
                         <MdOutlineCancel className={"text-white text-[35px]"}/>
@@ -23,9 +35,17 @@ export default function Detail({postDetails} : {postDetails : Video}){
                 </div>
                 <div className={"relative"}>
                     <div className={"lg:h-[100vh] h-[60vh]"}>
-                        <video src={post.video.asset.url}>
-
+                        <video src={post.video.asset.url}
+                               ref={videoRef} loop onClick={playVid}
+                               className={"h-full cursor-pointer"}>
                         </video>
+                    </div>
+                    <div className={"absolute top-[45%] left-[45%] cursor-pointer"}>
+                        {!isPlaying && (
+                            <button onClick={playVid}>
+                                <BsFillPlayFill className={"text-white text-6xl lg:text-8xl"}/>
+                            </button>
+                        )}
                     </div>
                 </div>
             </div>
