32cf27f40a9a37e83685ac4b5b96a742ceb46db6
xavier <xaviernguyen07@gmail.com>
2022-12-17 23:22:37 -0800

    added like functionality2

--- components/LikeButton.tsx
+++ components/LikeButton.tsx
@@ -20,7 +20,7 @@ export default function LikeButton({handleLike, handleDislike, likes} : IProps){
     }, [filterLikes, likes])
 
     return (
-        <div className={"gap-6"}>
+        <div className={"flex gap-6"}>
             <div className={"mt-4 flex flex-col justify-center items-center cursor-pointer"}>
                 {alreadyLiked ? (
                     <div className={"bg-primary rounded-full p-2 md:p-4 text-[#F51997]"}
