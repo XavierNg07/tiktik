e5c3f668fa625e172ad4e0f6a398a6c38c651b54
xavier <xaviernguyen07@gmail.com>
2022-12-17 23:31:48 -0800

    added NoResults page

--- components/NoResults.tsx
+++ components/NoResults.tsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import {MdOutlineVideocamOff} from 'react-icons/md';
+import {BiCommentX} from 'react-icons/bi';
 
 export default function NoResults({text} : {text : string}){
     return (
-        <div>
-
+        <div className={"flex flex-col justify-center items-center h-full w-full"}>
+            <p className={"text-8xl"}>
+                {text === "No comments yet!" ? (
+                    <BiCommentX/>
+                ) : (<MdOutlineVideocamOff/>)
+                }
+            </p>
+            <p className={"text-2xl text-center"}>{text}</p>
         </div>
     );
 }
