3aec29339a658a432408b50b2d8d81a7e14493e4
xavier <xaviernguyen07@gmail.com>
2022-12-18 00:24:07 -0800

    fetch all users

--- store/authStore.ts
+++ store/authStore.ts
@@ -1,17 +1,26 @@
 import create from 'zustand';
 import {persist} from 'zustand/middleware';
 import {IUser} from '../types';
+import {BASE_URL} from '../utils';
+import axios from 'axios';
 
 interface AuthState {
-    userProfile: IUser | null
-    addUser: (user : IUser) => void
-    removeUser: () => void
+    userProfile: IUser | null,
+    allUsers: IUser[]
+    addUser: (user : IUser) => void,
+    removeUser: () => void,
+    fetchAllUsers: () => void
 }
 export default create<AuthState>()(
     persist(set => ({
         userProfile : null,
+        allUsers : [],
         addUser: (user : IUser) => set({userProfile : user}),
         removeUser: () => set({userProfile : null}),
+        fetchAllUsers: async () => {
+            const {data} = await axios.get(`${BASE_URL}/api/users`);
+            set({allUsers : data});
+        }
     }), {name : 'auth'})
 )
 
