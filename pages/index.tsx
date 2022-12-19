dbf2d10bd57c22009452f6febcc20ec7a9ea48fb
xavier <xaviernguyen07@gmail.com>
2022-12-19 02:50:27 -0800

    testing

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,7 +5,7 @@ import VideoCard from '../components/VideoCard';
 import {BASE_URL} from '../utils';
 
 export default function Home({videos} : {videos : Video[]}) {
-  // console.log(videos);
+  console.log(videos);
   return (
     <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (videos.map((video: Video) => (<VideoCard post={video} key={video._id}/>)))
