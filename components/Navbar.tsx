f1f958cb65c2e34479fb8d83440102d41d9124d8
xavier <xaviernguyen07@gmail.com>
2022-12-18 21:16:51 -0800

    added search UI

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import Image from 'next/image';
 import Link from 'next/link';
 import {GoogleLogin, googleLogout} from '@react-oauth/google';
@@ -7,9 +7,19 @@ import {IoMdAdd} from 'react-icons/io';
 import Logo from '../utils/tiktik-logo.png';
 import createOrGetUser from '../utils';
 import useAuthStore from '../store/authStore';
+import {BiSearch} from "react-icons/all";
+import {useRouter} from "next/router";
 
 export default function Navbar(){
     const {userProfile, addUser, removeUser} = useAuthStore();
+    const [searchInput, setSearchInput] = useState('');
+    const router = useRouter();
+    function handleSearch(e : { preventDefault : () => void}){
+        e.preventDefault();
+        if (searchInput){
+            router.push(`/search/${searchInput}`);
+        }
+    }
 
     return (
         <div className="w-full flex justify-between items-center border-b-2 border-gray-200 py-2 px-4">
@@ -21,7 +31,19 @@ export default function Navbar(){
                     />
                 </div>
             </Link>
-            <div>SEARCH</div>
+            <div className={"relative hidden md:block"}>
+                <form onSubmit={handleSearch}
+                      className={"absolute md:static top-10 -left-20 bg-white"}>
+                    <input type={"text"} value={searchInput} onChange={(e) => setSearchInput(e.target.value)} placeholder={"Search accounts/videos"}
+                           className={"bg-primary p-3 md:text-md font-medium border-2 border-gray-100 " +
+                               "focus:outline-none focus:border-2 focus:border-gray-300 w-[300px] " +
+                               "md:w-[350px] rounded-full md:top-0"}/>
+                    <button onClick={handleSearch}
+                            className={"absolute md:right-5 right-6 top-4 border-l-2 border-gray-300 pl-4 text-2xl text-gray-400"}>
+                        <BiSearch/>
+                    </button>
+                </form>
+            </div>
             <div>
                 {userProfile ? (
                     <div className='flex gap-5 md:gap-10'>
