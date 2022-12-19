5968895374b7866b2b2027a8473599286ca299ef
xavier <xaviernguyen07@gmail.com>
2022-12-18 23:08:25 -0800

    added topic

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,6 @@ import {BASE_URL} from '../utils';
 
 export default function Home({videos} : {videos : Video[]}) {
   // console.log(videos);
-
   return (
     <div className="flex flex-col gap-10 videos h-full">
       {videos.length ? (videos.map((video: Video) => (<VideoCard post={video} key={video._id}/>)))
@@ -15,11 +14,16 @@ export default function Home({videos} : {videos : Video[]}) {
   );
 }
 
-export const getServerSideProps = async() => {
-  const {data} = await axios.get(`${BASE_URL}/api`);
+export async function getServerSideProps({query : {topic}} : {query : {topic : string}}){
+  let response;
+  if (topic){
+    response = await axios.get(`${BASE_URL}/api/discover/${topic}`);
+  } else {
+    response = await axios.get(`${BASE_URL}/api`);
+  }
   return {
     props: {
-      videos: data
+      videos: response.data
     }
   }
 }
